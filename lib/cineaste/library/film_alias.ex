224cc18af16281c3bb7b36d3a2009dd1eb96259d
James Tortorelli <jtortorelli03@icloud.com>
2025-03-15 10:04:15 -0500

    drop film aliases field

--- lib/cineaste/library/film_alias.ex
+++
@@ -1,9 +0,0 @@
-defmodule Cineaste.Library.FilmAlias do
-  use Ash.Resource,
-    data_layer: :embedded
-
-  attributes do
-    attribute :title, :string, allow_nil?: false
-    attribute :context, :string, allow_nil?: false
-  end
-end
